20a26ead8e9c0e20b4dfd12900e2444e98177406
Evan Klein <eklein217@gmail.com>
2024-01-02 14:01:37 -0500

    Add project files.

---
+++ Patterns/Singleton/Singleton.cs
@@ -0,0 +1,24 @@
+﻿namespace c_sharp_design_patterns.Patterns.Singleton;
+
+/// <summary>
+/// https://refactoring.guru/design-patterns/singleton
+/// </summary>
+internal class Singleton
+{
+    private static Singleton _instance;
+
+    private Singleton() { /* Implement instantiation logic here. */ }
+
+    public static Singleton Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+
+            return _instance;
+        }
+    }
+}
